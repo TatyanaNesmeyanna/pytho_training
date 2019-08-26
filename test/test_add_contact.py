0397db1f340308e7d1b6012557d07badc78551c6
Tatyana140589 <tatyana.kalimullina@abbyy.com>
2019-08-26 22:38:32 +0300

    GroupHelper and ContactHelper were described

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,14 +13,14 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="FirstName", middlename="Middlename", lastname="Lastname", nickname="Nickname", title="Title", company="Company", address="Address", home="1",
-                        mobile_phone="777777", work_phone="666666", fax="555555", email="test@mail.ru", adress2="test adress", phone2="1", notes="no"))
+    app.contact.create(Contact(first_name="FirstName", middlename="Middlename", lastname="Lastname", nickname="Nickname", title="Title", company="Company", address="Address", home="1",
+                               mobile_phone="777777", work_phone="666666", fax="555555", email="test@mail.ru", adress2="test adress", phone2="1", notes="no"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="", middlename="", lastname="", nickname="", title="", company="", address="", home="",
-                        mobile_phone="", work_phone="", fax="", email="", adress2="", phone2="", notes=""))
+    app.group.create(Contact(first_name="", middlename="", lastname="", nickname="", title="", company="", address="", home="",
+                             mobile_phone="", work_phone="", fax="", email="", adress2="", phone2="", notes=""))
     app.session.logout()
 
