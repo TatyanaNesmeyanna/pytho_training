4aabf98e5f186e41769ac391e76f5346421d2e8f
Tatyana140589 <tatyana.kalimullina@abbyy.com>
2019-08-28 15:48:47 +0300

    modified fill_group_form method described new method change_field value modify group name, group header tests were added

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -6,3 +6,15 @@ def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="modified_name", header="modified_header", footer="modified_footer"))
     app.session.logout()
+
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="second_modified_name"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="second_modified_header"))
+    app.session.logout()
