bdfbe50d1812a7e66bd361e3587362c726a00b93
Tatyana140589 <tatyana.kalimullina@abbyy.com>
2019-08-28 21:47:49 +0300

    Login and logout was removed from tests Conftest.py was modified Fixture was divided into n2 parts for login and logout

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,15 +3,11 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(first_name="FirstName", middlename="Middlename", lastname="Lastname", nickname="Nickname", title="Title", company="Company", address="Address", home="1",
-                               mobile_phone="777777", work_phone="666666", fax="555555", email="test@mail.ru", adress2="test adress", phone2="1", notes="no"))
-    app.session.logout()
+                               mobile_phone="777777", work_phone="666666", fax="555555", email="test@mail.ru", address2="test adress", phone2="1", notes="no"))
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(first_name="", middlename="", lastname="", nickname="", title="", company="", address="", home="",
-                             mobile_phone="", work_phone="", fax="", email="", adress2="", phone2="", notes=""))
-    app.session.logout()
+                             mobile_phone="", work_phone="", fax="", email="", address2="", phone2="", notes=""))
 
