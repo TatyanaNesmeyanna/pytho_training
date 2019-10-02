105b10fd849d9d5b1bef34e5b5afb00fc58507ac
Tatyana140589 <tatyana.kalimullina@abbyy.com>
2019-10-02 19:30:19 +0300

    added data package with data for contacts and groups

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-def random_phone_number():
-    n_groups = random.randint(3, 6)
-    symbols = string.digits + "-"
-    return ('-'.join([''.join(random.choices(symbols, k=random.randint(1, 3))) for _ in range(n_groups)]))
-
-testdata=[Contact(firstname="", lastname="", home_phone="",
-                               mobile_phone="", work_phone="", secondary_phone="")] + [Contact(firstname=random_string("FirstName", 10), lastname=random_string("LastName", 10), home_phone=random_phone_number(),
-                               mobile_phone=random_phone_number(), work_phone=random_phone_number(), secondary_phone=random_phone_number()) for i in range(5)]
+from data.add_contact import constant as testdata
 
 @pytest.mark.parametrize('contact', testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
