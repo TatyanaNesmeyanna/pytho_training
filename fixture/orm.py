072e54d7981026b76293812a6fe498adfee6f620
Tatyana140589 <tatyana.kalimullina@abbyy.com>
2019-10-08 18:58:38 +0300

    added test for all contacts data

--- fixture/orm.py
+++ fixture/orm.py
@@ -13,16 +13,25 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda : ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
         id = PrimaryKey(int, column='id')
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
+        home_phone = Optional(str, column='home')
+        mobile_phone = Optional(str, column='mobile')
+        work_phone = Optional(str, column='work')
+        secondary_phone= Optional(str, column='phone2')
+        email = Optional(str, column='email')
+        email2 = Optional(str, column='email2')
+        email3 = Optional(str, column='email3')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda : ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts", lazy=True)
 
     def __init__(self, host, name, user, password):
-        self.db.bind('mysql',host = host, database = name, user = user, password = password)
+        self.db.bind('mysql',host = host, database = name, user = user, password = password )
         self.db.generate_mapping()
         sql_debug(True)
 
@@ -33,7 +42,9 @@ class ORMFixture:
 
     def convert_contacts_to_model(self, contacts):
         def convert(contact):
-            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
+            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname, home_phone=contact.home_phone,
+                           mobile_phone=contact.mobile_phone, work_phone=contact.work_phone,secondary_phone=contact.secondary_phone,
+                           email1=contact.email, email2=contact.email2, email3=contact.email3)
         return list(map(convert, contacts))
 
     @db_session
@@ -43,3 +54,14 @@ class ORMFixture:
     @db_session
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group=list(select(g for g in ORMFixture.ORMGroup if g.id==group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
---
+++ test/test_check_all_contacts_data.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from fixture.orm import ORMFixture
+from model.contact import Contact
+import re
+
+
+def test_check_all_contacts_data(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="firstname", lastname="lastname", address="address",
+                                    home_phone="123", work_phone="456", mobile_phone="789", secondary_phone="135",
+                                        email1="1@gmail.com", email2="2@gmail.com", email3="3@gmail.com"))
+    contacts_from_homepage = sorted(app.contact.get_contact_list(),key=Contact.id_or_max)
+    db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for c in contacts_from_db:
+        c.all_phones_from_home_page = merge_phones_like_on_home_page(c)
+        c.all_emails_from_home_page = merge_emails_like_on_home_page(c)
+    list_of_phones_from_db=[]
+    list_of_emails_from_db=[]
+    list_of_phones_from_home_page = []
+    list_of_emails_from_home_page = []
+    for i in range(0, len(contacts_from_db)):
+        list_of_phones_from_db.append(contacts_from_db[i].all_phones_from_home_page)
+        list_of_emails_from_db.append(contacts_from_db[i].all_emails_from_home_page)
+        list_of_phones_from_home_page.append(contacts_from_homepage[i].all_phones_from_home_page)
+        list_of_emails_from_home_page.append(contacts_from_homepage[i].all_emails_from_home_page)
+    assert contacts_from_db == contacts_from_homepage
+    assert list_of_phones_from_db == list_of_phones_from_home_page
+    assert list_of_emails_from_db == list_of_emails_from_home_page
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x : x!="",
+                            map(lambda x : clear(x),
+                                filter(lambda x : x is not None,
+                                       [contact.home_phone, contact.mobile_phone, contact.work_phone, contact.secondary_phone]))))
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            filter(lambda x: x is not None,
+                                       [contact.email1, contact.email2, contact.email3])))
